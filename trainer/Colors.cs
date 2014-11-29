4b834863134343662680f3abd9b7e3491c97da07
smg <semagerasimov@gmail.com>
2014-11-29 16:24:06 +0700

    цвета клавиш

--- trainer/Colors.cs
+++ trainer/Colors.cs
@@ -9,7 +9,7 @@ namespace trainer
     public static class Colors
     {
         // буквы в текстовых полях
-        public static Color wrongLetterBackground  = Color.Chocolate;
+        public static Color wrongLetterBackground = Color.Chocolate;
         public static Color passedLetterBackground = Color.Gray;
         public static Color clearLetterBackground = Color.White;
         public static Color wrongLetter = Color.White;
@@ -17,19 +17,20 @@ namespace trainer
         public static Color clearLetter = Color.Black;
 
         // надписи на клавишах
-        public static Color keyLabel            = Color.Black;
+        public static Color keyLabel = Color.Black;
         public static Color keyLabelHighlighted = Color.White;
 
         // фон клавиш
-        public static Color common = Color.FromArgb(60, 200, 60);
+        public static Color common = Color.FromArgb(180, 180, 180);
 
-        private static Color firstFinger       = Color.FromArgb(60, 200, 60);
-        private static Color secondLeftFinger  = Color.FromArgb(60, 200, 60);
-        private static Color secondRightFinger = Color.FromArgb(60, 200, 60);
-        private static Color thirdFinger       = Color.FromArgb(60, 200, 60);
-        private static Color fourthFinger      = Color.FromArgb(60, 200, 60);
-        private static Color fifthFinger       = Color.FromArgb(60, 200, 60);
-        
+        // from https://color.adobe.com/ru/Flat-rainbow-color-theme-3012612
+        private static Color firstFinger = Color.FromArgb(180, 180, 180);
+        private static Color secondLeftFinger = Color.FromArgb(241, 90, 90);
+        private static Color secondRightFinger = Color.FromArgb(240, 196, 25);
+        private static Color thirdFinger = Color.FromArgb(78, 186, 111);
+        private static Color fourthFinger = Color.FromArgb(45, 149, 191);
+        private static Color fifthFinger = Color.FromArgb(149, 91, 165);
+
         public static Color OfFinger(Fingers finger)
         {
             switch (finger)
